13093f72323049f1182400e39354f9f802b7d993
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2022-05-16 14:16:42 +0200

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -11,22 +11,6 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-int	ft_sign(char *str)
-{
-	int	i;
-	int	sign;
-
-	sign = 1;
-	i = 0;
-	while (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			sign = -sign;
-		i++;
-	}
-	return (sign);
-}
-
 int	ft_atoi(const char *str)
 {
 	int			sign;
