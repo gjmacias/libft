a3382e05a0fb484d6a6ee09719163854da479320
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2022-05-16 14:18:26 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -50,6 +50,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+int		ft_sign(char *str);
 // Funciones malloc
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
@@ -75,4 +76,5 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+//extras
 #endif
