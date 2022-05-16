28f7dfa953d085e732018a617e53014af7701155
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2022-05-16 14:45:24 +0200

    Update ft_error.c

--- ft_error.c
+++ ft_error.c
@@ -1,14 +1,14 @@
 void	ft_error(int nerror)
 {
-	if (nerror = 404)
-		ft_putstr_fd("maeaefaef", 1);
-	if (nerror = 404)
-		ft_putstr_fd("maeaefaef", 1);
-	if (nerror = 404)
-		ft_putstr_fd("maeaefaef", 1);
-	if (nerror = 404)
-		ft_putstr_fd("maeaefaef", 1);
-	if (nerror = 404)
-		ft_putstr_fd("maeaefaef", 1);
+	if (nerror = 1)
+		ft_putstr_fd("Ops! Malloc failed!", 1);
+	if (nerror = 2)
+		ft_putstr_fd("you need more parameters!!", 1);
+	if (nerror = 3)
+		ft_putstr_fd("the parameters are not correct type -h for help", 1);
+	if (nerror = 4)
+		ft_putstr_fd("bye", 1);
+	if (nerror = 5)
+		ft_putstr_fd("hello", 1);
 	exit(EXIT_FAILURE);
 }
