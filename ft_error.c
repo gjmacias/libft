b5908a0faa7193a67bae40d9dcdff04df5d26639
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2022-05-16 14:33:27 +0200

    Create ft_error.c

---
+++ ft_error.c
@@ -0,0 +1,14 @@
+void	ft_error(int nerror)
+{
+	if (nerror = 404)
+		ft_putstr_fd("maeaefaef", 1);
+	if (nerror = 404)
+		ft_putstr_fd("maeaefaef", 1);
+	if (nerror = 404)
+		ft_putstr_fd("maeaefaef", 1);
+	if (nerror = 404)
+		ft_putstr_fd("maeaefaef", 1);
+	if (nerror = 404)
+		ft_putstr_fd("maeaefaef", 1);
+	exit(EXIT_FAILURE);
+}
